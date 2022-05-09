39197cbbe74c677459f6496da93b60ef54166d1e
wilsonmesquita03 <wilsonbrasilplauforever@gmial.com>
2022-05-09 11:19:49 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,10 @@
+//Alterando H1
+let titulo = document.querySelector('h1')
+
+titulo.innerHTML = "Olá Mundo"
+
+//Imprimindo no console pelo id
+console.log(document.querySelector('#par01'))
+
+//Imprimindo primeira classe tittle do html 
+console.log(document.querySelector('.tittle'))
